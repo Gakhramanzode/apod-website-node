3582673ae1d90e700b6e6222ace8ebdb3226cfcf
Asker Kakhramanov <asker.kakhramanov@me.com>
2023-04-20 17:12:18 +0300

    modified test/test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,20 @@
-const assert = require('assert');
-const request = require('supertest');
-const server = require('../index');
+const assert = require('assert'); // Подключаем модуль assert для проверки утверждений
+const request = require('supertest'); // Подключаем модуль supertest для тестирования HTTP-запросов
+const server = require('../index'); // Подключаем веб-сайта
 
+// Описываем набор тестов с помощью функции describe
 describe('GET /', function() {
+  // Описываем конкретный тест с помощью функции it
   it('responds with status code 200', function(done) {
+    // Отправляем GET-запрос на главную страницу веб-сайта
     request(server)
       .get('/')
+      // Проверяем, что сервер отвечает с кодом состояния 200
       .expect(200, done);
   });
 });
 
+// Функция для автоматического закрытия сервера после завершения тестов
 after(function(done) {
   server.close(done);
 });
