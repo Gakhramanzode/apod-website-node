37ea2ff7a88e331d51457ab82e5fb9a6add0a446
Asker Kakhramanov <asker.kakhramanov@me.com>
2023-04-20 15:19:34 +0300

    modified package-lock.json package.json test

---
+++ test/apod.test.js
@@ -0,0 +1,20 @@
+// Импортируем модули
+const chai = require('chai');
+const http = require('http');
+
+// Создаем переменную expect
+const expect = chai.expect;
+
+// Определяем группу тестов
+describe('Server', function() {
+  // Определяем один тест
+  it('should start and respond to requests', function(done) {
+    // Отправляем GET-запрос на сервер
+    http.get('http://localhost:55360', function(response) {
+      // Проверяем статус ответа
+      expect(response.statusCode).to.equal(200);
+      // Завершаем тест
+      done();
+    });
+  });
+});
